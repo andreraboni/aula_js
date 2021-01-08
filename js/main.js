23f9f98beb232ab28c1ee59822098bc1ff5ca794
Andre Raboni <andreraboni@gmail.com>
2021-01-07 23:05:28 -0300

    Adiciona arquivo JavaScript

---
+++ js/main.js
@@ -0,0 +1,52 @@
+/*
+var nome1 = "André";
+var nome2 = "Carol";
+var idade = 39;
+var idade2 = 32;
+var frase = "O meu nome é André"
+// alert(nome + " tem " + idade + " anos.");
+ console.log("nome: " + nome1);
+console.log("nome: " + nome2);
+console.log("idade: " + idade);
+console.log("idade: " + idade2);
+soma = parseInt(idade) + parseInt(idade2);
+console.log("A soma da idade de " + nome1 + " e de " + nome2 + " é igual a " + (soma));
+
+console.log(idade / idade2);
+
+var lista = ["maça", "pera", "laranja"];
+lista.pop();
+console.log(lista);
+
+
+/*
+var jogadores = [{nome: "Neymar", altura: "1.78", time: "PSG"}, 
+                 {nome: "Pelé", altura: "1.80", time: "Santos"},
+                 {nome: "Johny", altura: "1.69", time: "Palmeiras"}]; 
+console.log(jogadores[2].nome + " - " + jogadores[2].altura);
+
+var idade = prompt("Informe a sua idade: ");
+if (idade <= 18){
+    alert("Você é menor de idade");
+}else{
+    alert("Você é maior de idade");
+};
+
+var count = 0;
+while(count <= 10){
+    console.log(count);
+    count++;
+}
+
+var count;
+for (count = 0; count <= 10; count++){
+    console.log(count);
+};
+
+
+var d = new Date();
+console.log(d.getFullYear());*/
+
+var f1 = "isso";
+var f2 = "aquilo";
+console.log(f1 + f2 "1439" + "-" + "1545");
